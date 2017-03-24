3e60bf22fc5ac513a68b36eeac4338f27dc1a4ba
Rafael Villarreal <rvillarreal416@gmail.com>
2017-03-24 12:11:21 -0500

    better syntax with lodash

--- src/components/app.js
+++ src/components/app.js
@@ -18,11 +18,18 @@ const todos = [
 
 
 class App extends React.Component{
+	constructor(props){
+		super(props)
+
+		this.state = {
+			todos
+		}
+	}
 	render(){
 		return(
 			<div>
 				<h1>React To Do App </h1>
-				<TodoList />
+				<TodoList todos={this.state.todos}/>
 			</div>
 		)
 	}
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import TodoListHeader from './todo-list-header'
+import _ from 'lodash'
+
+
 
 class TodoList extends React.Component{
+
+	renderItems(){
+		return _.map()
+	}
+
 	render(){
+		console.log(this.props.todos)
 		return(
 			<table>
-				<thead>
-					<tr>
-						<th>Task</th>
-						<th>Action</th>
-					</tr>
-				</thead>
+				<TodoListHeader />
+				<tr>
+					{this.renderItems()}
+				</tr>
 			</table>
 		)
 	}
